1b1a2f909dc5b3c94fef8ed5f50790e919bcc436
Mateus Caltabiano <mateus.frauzino@gmail.com>
2023-06-05 08:46:33 -0300

    adicionando esqueleto do controller

---
+++ docs/PadroesDeProjeto/GRASPs/Controller.js
@@ -0,0 +1,29 @@
+class Controller {
+  constructor() {
+    this.carrinho = new carrinho();
+  }
+
+  fazerLogin(autenticacao) {}
+
+  acessarPerfil(usuario) {}
+
+  fazerLogout(usuario) {}
+
+  buscarProduto(busca) {}
+
+  visualizarProduto(produto) {}
+
+  adicionarAListaDeDesejos(produto) {}
+
+  removerDaListaDeDesejos(produto) {}
+
+  visualizarListaDeDesejos(produto) {}
+
+  adicionarAoCarrinho(produto) {}
+
+  removerDoCarrinho(produto) {}
+
+  visualizarCarrinho() {}
+
+  iniciarCheckout() {}
+}
